a06087d50f0a9baca0bdb4166ee9f3eedd8110a6
Lauren Leslie Ferreira <laurenlesliesilva@gmail.com>
2025-02-19 10:10:42 -0300

    chore: biomejs

--- src/server.ts
+++ src/server.ts
@@ -1,48 +1,48 @@
-import fastifyCors from "@fastify/cors";
-import fastifySwagger from "@fastify/swagger";
-import fastifySwaggerUi from "@fastify/swagger-ui";
-import fastify from "fastify";
+import fastifyCors from "@fastify/cors"
+import fastifySwagger from "@fastify/swagger"
+import fastifySwaggerUi from "@fastify/swagger-ui"
+import fastify from "fastify"
 import {
-  jsonSchemaTransform,
-  serializerCompiler,
-  validatorCompiler,
-  ZodTypeProvider,
-} from "fastify-type-provider-zod";
-import { env } from "./env";
-import { healthRoute } from "./routes/health";
-import { subscribeRoute } from "./routes/subscribe";
+	type ZodTypeProvider,
+	jsonSchemaTransform,
+	serializerCompiler,
+	validatorCompiler,
+} from "fastify-type-provider-zod"
+import { env } from "./env"
+import { healthRoute } from "./routes/health"
+import { subscribeRoute } from "./routes/subscribe"
 
-const app = fastify().withTypeProvider<ZodTypeProvider>();
+const app = fastify().withTypeProvider<ZodTypeProvider>()
 
-app.setSerializerCompiler(serializerCompiler);
-app.setValidatorCompiler(validatorCompiler);
+app.setSerializerCompiler(serializerCompiler)
+app.setValidatorCompiler(validatorCompiler)
 
 app.register(fastifyCors, {
-  origin: "*",
-});
+	origin: "*",
+})
 
 app.register(fastifySwagger, {
-  openapi: {
-    info: {
-      title: "NLW Connect - Node.js",
-      version: "0.0.1",
-    },
-  },
-  transform: jsonSchemaTransform,
-});
+	openapi: {
+		info: {
+			title: "NLW Connect - Node.js",
+			version: "0.0.1",
+		},
+	},
+	transform: jsonSchemaTransform,
+})
 
 app.register(fastifySwaggerUi, {
-  routePrefix: "/docs",
-});
+	routePrefix: "/docs",
+})
 
-app.register(healthRoute);
+app.register(healthRoute)
 
-app.register(subscribeRoute);
+app.register(subscribeRoute)
 
-app.listen({ port: env.PORT }, (err) => {
-  if (err) {
-    console.error(err);
-    process.exit(1);
-  }
-  console.log(`Server listening at http://localhost:${env.PORT} 🚀`);
-});
+app.listen({ port: env.PORT }, err => {
+	if (err) {
+		console.error(err)
+		process.exit(1)
+	}
+	console.log(`Server listening at http://localhost:${env.PORT} 🚀`)
+})
