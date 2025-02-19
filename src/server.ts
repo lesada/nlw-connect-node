9d7de11facc06fc8916093fe3be7bfa962f3df14
Lauren Leslie Ferreira <laurenlesliesilva@gmail.com>
2025-02-19 09:58:02 -0300

    big bang: initial commit

---
+++ src/server.ts
@@ -0,0 +1,47 @@
+import fastifyCors from "@fastify/cors";
+import fastifySwagger from "@fastify/swagger";
+import fastifySwaggerUi from "@fastify/swagger-ui";
+import fastify from "fastify";
+import {
+  jsonSchemaTransform,
+  serializerCompiler,
+  validatorCompiler,
+  ZodTypeProvider,
+} from "fastify-type-provider-zod";
+import { healthRoute } from "./routes/health";
+import { subscribeRoute } from "./routes/subscribe";
+
+const app = fastify().withTypeProvider<ZodTypeProvider>();
+
+app.setSerializerCompiler(serializerCompiler);
+app.setValidatorCompiler(validatorCompiler);
+
+app.register(fastifyCors, {
+  origin: "*",
+});
+
+app.register(fastifySwagger, {
+  openapi: {
+    info: {
+      title: "NLW Connect - Node.js",
+      version: "0.0.1",
+    },
+  },
+  transform: jsonSchemaTransform,
+});
+
+app.register(fastifySwaggerUi, {
+  routePrefix: "/docs",
+});
+
+app.register(healthRoute);
+
+app.register(subscribeRoute);
+
+app.listen({ port: 3333 }, (err) => {
+  if (err) {
+    console.error(err);
+    process.exit(1);
+  }
+  console.log(`Server listening at http://localhost:3333 🚀`);
+});
