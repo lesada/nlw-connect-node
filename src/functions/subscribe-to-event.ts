5f0895164c8d0e35549b273fb464cb91bf041fa7
Lauren Leslie Ferreira <laurenlesliesilva@gmail.com>
2025-02-19 14:53:06 -0300

    feat: subscribe to event function

---
+++ src/functions/subscribe-to-event.ts
@@ -0,0 +1,25 @@
+import { db } from "../drizzle/client"
+import { subscriptions } from "../drizzle/schema/subscriptions"
+
+type subscribeToEventParams = {
+	name: string
+	email: string
+}
+
+export async function subscribeToEvent({
+	name,
+	email,
+}: subscribeToEventParams) {
+	const result = await db
+		.insert(subscriptions)
+		.values({
+			name,
+			email,
+		})
+		.returning()
+
+	const subscriber = result[0]
+	return {
+		subscriberId: subscriber.id,
+	}
+}
--- src/routes/subscribe.ts
+++ src/routes/subscribe.ts
@@ -1,5 +1,6 @@
 import type { FastifyPluginAsyncZod } from "fastify-type-provider-zod"
 import z from "zod"
+import { subscribeToEvent } from "../functions/subscribe-to-event"
 
 export const subscribeRoute: FastifyPluginAsyncZod = async app => {
 	app.post(
@@ -14,15 +15,16 @@ export const subscribeRoute: FastifyPluginAsyncZod = async app => {
 				tags: ["subscription"],
 				response: {
 					201: z.object({
-						name: z.string(),
-						email: z.string().email(),
+						subscriberId: z.string(),
 					}),
 				},
 			},
 		},
 		async (req, rep) => {
 			const { name, email } = req.body
-			rep.status(201).send({ name, email })
+
+			const { subscriberId } = await subscribeToEvent({ name, email })
+			rep.status(201).send({ subscriberId })
 		}
 	)
 }
